7445f654db9eeabff3bd2a6c4bed01267af29263
Elton Mario da Silva <eltonjf@gmail.com>
2019-01-09 16:52:54 -0200

    Configuração com Firebase e salvando categorias

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
+
 
 const port = process.env.PORT || 3000
 
-let i = 10
-app.get('/', (req, response) => {
-    i++
-    response.send(`<h1>Olá Fullstack Lab ${i}</h1>`)
+
+app.get('/', async(reqquest, response) => {
+    const content = await axios.get('https://como-fazer-pestudo.firebaseio.com/teste.json')
+    console.log(content.data)
+    response.render('index', { i: content.data })
+})
+
+app.get('/categorias/nova', (req, res) => {
+    res.render('categorias/nova')
+})
+
+app.post('/categorias/nova', async(req, res) =>{
+    await axios.post('https://como-fazer-pestudo.firebaseio.com/teste.json',  {
+        categoria: req.body.categoria
+    })
+    res.send(req.body)
 })
 
 app.listen(port, (err) => {
